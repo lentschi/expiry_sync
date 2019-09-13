ed3f8c7a320a292b1524cc7e3c0d465c022fcafc
Florian Lentsch <office@florian-lentsch.at>
2019-09-13 10:10:43 +0200

    Adapted build scripts

--- setup/app/build_scripts/build-android-release.sh
+++ setup/app/build_scripts/build-android-release.sh
@@ -1,50 +1,23 @@
 #!/bin/bash -ve
 
-if [ "$#" -ne 2 ]; then
-    echo "USAGE: build-android-prod <version-code> <default-host>"
-    exit 1
-fi
 
-
-rm -Rf /tmp/out || true
-mkdir /tmp/out
 export ANDROID_HOME=/opt/android-sdk-linux
 export ANDROID_SDK=/opt/android-sdk-linux
 
-source "/root/.sdkman/bin/sdkman-init.sh"
+source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-# Crosswalk
 cd /srv/project
+rm platforms/android/app/build/outputs/apk/release/* || true
 cp -p config.xml /tmp
 cp -p package.json /tmp
-cp -p npm-shrinkwrap.json /tmp
-cp -p src/app/models/setting.ts /tmp
-ionic cordova platform remove android || true
-ionic cordova plugin add cordova-plugin-crosswalk-webview@2.3.0
-ionic cordova platform add android
-/srv/config/build_scripts/adept_config_xml.rb /srv/project/config.xml "$1" --crosswalk
-export LC_ALL=C.UTF-8
-/srv/config/build_scripts/adept_default_host_setting.rb /srv/project/src/app/models/setting.ts "$2"
-ionic cordova build android --prod --release
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore deploy-keys/expiry-sync.keystore platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk expiry_sync_client
-$ANDROID_HOME/build-tools/26.0.0/zipalign  -v 4 platforms/android/build/outputs/apk/android-armv7-release-unsigned.apk platforms/android/build/outputs/apk/expiry-sync-signed-crosswalk.apk
-cp platforms/android/build/outputs/apk/expiry-sync-signed-crosswalk.apk /tmp/out
-ionic cordova plugin remove cordova-plugin-crosswalk-webview
+cp -p package-lock.json /tmp
 ionic cordova platform remove android
 ionic cordova platform add android
 cp -p /tmp/config.xml ./
-
-# Non-crosswalk
-/srv/config/build_scripts/adept_config_xml.rb /srv/project/config.xml "$1"
 ionic cordova build android --prod --release
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore deploy-keys/expiry-sync.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk expiry_sync_client
-$ANDROID_HOME/build-tools/26.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk platforms/android/build/outputs/apk/expiry-sync-signed.apk
-cp platforms/android/build/outputs/apk/expiry-sync-signed.apk /tmp/out
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore deploy-keys/expiry-sync.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk expiry_sync_client
+$ANDROID_HOME/build-tools/28.0.3/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk platforms/android/app/build/outputs/apk/release/app-release-signed.apk
 cp -p /tmp/config.xml ./
 cp -p /tmp/package.json ./
-cp -p /tmp/npm-shrinkwrap.json ./
-cp -p /tmp/setting.ts ./src/app/models
-
-rm platforms/android/build/outputs/apk/*
-cp /tmp/out/* platforms/android/build/outputs/apk
-rm -Rf /tmp/out
+cp -p /tmp/package-lock.json ./
+ls -lah platforms/android/app/build/outputs/apk/release/app-release-signed.apk
